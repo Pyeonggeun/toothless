df1c1ba19ce9b911713566c16ffbffcd5deb8f4d
duckggkk <rlawhdals015@gmail.com>
2023-12-01 19:48:46 +0900

    20231201 d조 김종민 jsp파일수정

--- src/main/java/com/mkfactory/toothless/d/jm/consulting/controller/ConsultingController.java
+++ src/main/java/com/mkfactory/toothless/d/jm/consulting/controller/ConsultingController.java
@@ -1,16 +1,35 @@
 package com.mkfactory.toothless.d.jm.consulting.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mkfactory.toothless.d.dto.HopeJobDto;
+import com.mkfactory.toothless.d.jm.consulting.service.ConsultingService;
+import com.mkfactory.toothless.donot.touch.dto.StudentInfoDto;
 
 @Controller
 @RequestMapping("/t1_d/consulting/*")
 public class ConsultingController {
 	
+	@Autowired
+	private ConsultingService consultingService;
+	
+	
+	
+	//구직희망신청 insert
 	@RequestMapping("hopeJobApplyProcess")
-	//구직희망 dto넣기
-	public String insertHopeJobApply() {
+	public String insertHopeJobApply(HopeJobDto par, HttpSession session) {
 		
+		//학생정보 pk 입력
+		StudentInfoDto studentInfoDto = (StudentInfoDto)session.getAttribute("sessionStudentInfo");
+		par.setStudent_pk(studentInfoDto.getStudent_pk());
+		
+		
+		//구직희망신청 insert 실행
+		consultingService.insertHopeJobApply(par);
 		return "";
 	}
 }
--- src/main/java/com/mkfactory/toothless/d/jm/consulting/service/ConsultingService.java
+++ src/main/java/com/mkfactory/toothless/d/jm/consulting/service/ConsultingService.java
@@ -1,12 +1,19 @@
 package com.mkfactory.toothless.d.jm.consulting.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.mkfactory.toothless.d.dto.HopeJobDto;
+import com.mkfactory.toothless.d.jm.consulting.mapper.ConsultingMapper;
 
 @Service
 public class ConsultingService {
 	
+	@Autowired
+	private ConsultingMapper consultingMapper ;
+	
 	//dto넣기
-	public void insertHopeJobApply() {
-		
+	public void insertHopeJobApply(HopeJobDto par) {
+		consultingMapper.insertHopeJobApply(par);
 	}
 }
