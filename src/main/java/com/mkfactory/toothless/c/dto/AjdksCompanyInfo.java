e51adbf0fbbbb718a95be8f2e12fcd1274bc64b3
kimuzz <134681164+kimuzz@users.noreply.github.com>
2023-12-01 18:04:49 +0900

    오류테스트

---
+++ src/main/java/com/mkfactory/toothless/c/dto/AjdksCompanyInfo.java
@@ -0,0 +1,15 @@
+package com.mkfactory.toothless.c.dto;
+
+public class AjdksCompanyInfo {
+	
+	public int company_pk;
+	public int company_category_pk;
+	public int staff_pk;
+	public int external_pk;
+	public String company_id;
+	public String company_name;
+	public String ceo_name;
+	public String address;
+	public String phone;
+	public String url;
+}
