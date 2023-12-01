14f7dfa386d5bf5c0947ad425e53a2c237de9e2a
duckggkk <rlawhdals015@gmail.com>
2023-12-01 21:04:47 +0900

    20231201 d조 김종민 구직희망신청기능

--- src/main/java/com/mkfactory/toothless/d/jm/consulting/controller/ConsultingController.java
+++ src/main/java/com/mkfactory/toothless/d/jm/consulting/controller/ConsultingController.java
@@ -11,25 +11,39 @@ import com.mkfactory.toothless.d.jm.consulting.service.ConsultingService;
 import com.mkfactory.toothless.donot.touch.dto.StudentInfoDto;
 
 @Controller
-@RequestMapping("/t1_d/consulting/*")
+@RequestMapping("/tl_d/jm_consulting/*")
 public class ConsultingController {
 	
 	@Autowired
 	private ConsultingService consultingService;
 	
+
+	
+	//구직희망 신청서 등록 페이지
+	@RequestMapping("applyHopeJobPage")
+	public String applyHopeJobPage() {
+		return "tl_d/jm_consulting/applyHopeJobPage";
+	}
 	
 	
 	//구직희망신청 insert
 	@RequestMapping("hopeJobApplyProcess")
 	public String insertHopeJobApply(HopeJobDto par, HttpSession session) {
 		
-		//학생정보 pk 입력
+			
+			
+		//학생정보 pk 출력
 		StudentInfoDto studentInfoDto = (StudentInfoDto)session.getAttribute("sessionStudentInfo");
-		par.setStudent_pk(studentInfoDto.getStudent_pk());
 		
+		//구직희망 프로그램을 아직 진행중인 학생이라면 등록 거부
+		
+		
+		
+		//학생정보 pk 입력
+		par.setStudent_pk(studentInfoDto.getStudent_pk());
 		
 		//구직희망신청 insert 실행
 		consultingService.insertHopeJobApply(par);
-		return "";
+		return "redirect:./applyHopeJobPage";
 	}
 }
