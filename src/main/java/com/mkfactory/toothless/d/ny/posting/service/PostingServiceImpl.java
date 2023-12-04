e66d2f0ab9d6a3454765c6fc5516c53b9135ec97
LYN0831 <140958723+LYN0831@users.noreply.github.com>
2023-12-04 09:14:22 +0900

    나연 Service수정 jsp 2개 추가 1개 수정

--- src/main/java/com/mkfactory/toothless/d/ny/posting/service/PostingServiceImpl.java
+++ src/main/java/com/mkfactory/toothless/d/ny/posting/service/PostingServiceImpl.java
@@ -3,6 +3,6 @@ package com.mkfactory.toothless.d.ny.posting.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PostingServiceImpl {
+public interface postingServiceImpl {
 
 }
