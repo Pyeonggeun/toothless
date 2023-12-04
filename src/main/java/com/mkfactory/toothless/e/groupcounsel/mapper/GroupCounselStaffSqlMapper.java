e1bc4cae5a2bf2be102b9994587152a92b6c712c
Pyeonggeun <rjal3054@gmail.com>
2023-12-04 17:33:44 +0900

    오늘저녁메뉴

--- src/main/java/com/mkfactory/toothless/e/groupcounsel/mapper/GroupCounselStaffSqlMapper.java
+++ src/main/java/com/mkfactory/toothless/e/groupcounsel/mapper/GroupCounselStaffSqlMapper.java
@@ -1,11 +1,17 @@
 package com.mkfactory.toothless.e.groupcounsel.mapper;
 
+import java.util.List;
+
 import com.mkfactory.toothless.e.dto.GroupCounselDto;
 
 public interface GroupCounselStaffSqlMapper {
 	
+	// 집단상담 등록
 	public void insertGroupCounsel(GroupCounselDto groupCounselDto);
 	
+	// 등록된 상담 리스트
+	public List<GroupCounselDto> selectGroupCounselList();
 	
-	
+	// pk별 집단 상담
+	public GroupCounselDto selectGroupCounselById(int id);
 }
