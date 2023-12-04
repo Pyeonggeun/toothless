3b056b41413f43af0fb30f0b623d1f863b810faa
SeokE <100415173+whdudtjr@users.noreply.github.com>
2023-12-04 15:02:13 +0900

    관리자 컨트롤러,서비스,매퍼 수정
    관리자 컨트롤러,서비스,매퍼 수정

--- src/main/java/com/mkfactory/toothless/b/ys/item/mapper/ItemStaffSqlMapper.java
+++ src/main/java/com/mkfactory/toothless/b/ys/item/mapper/ItemStaffSqlMapper.java
@@ -1,5 +1,29 @@
 package com.mkfactory.toothless.b.ys.item.mapper;
 
-public class ItemStaffSqlMapper {
+import java.util.List;
+import java.util.Map;
 
+import com.mkfactory.toothless.b.dto.ItemCatDto;
+import com.mkfactory.toothless.b.dto.ItemDto;
+
+public interface ItemStaffSqlMapper {
+	
+	public List<ItemDto> selectItemList();
+	
+	public ItemDto selectItemByItemCategoryPk(int item_cat_pk);
+	
+	public ItemCatDto selectItemCatByItemCategoryPk(int item_cat_pk);
+	
+	public List<ItemCatDto> selectItemCategoryList();
+	
+	public void insertItem(ItemDto itemDto);
+	
+	public ItemDto selectItemByItemPk(int item_pk);
+	
+	public void updateItemByItemPk(ItemDto itemDto);
+	
+	public void deleteItemByItemPk(int item_pk);
+	
+	
+	public List<Map<String, Object>> selectItemJoinCategoryList();
 }
