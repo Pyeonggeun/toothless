9351438e8504a9409e25302d24fad6b143fcd9b7
sunmilk75 <145519399+sunmilk75@users.noreply.github.com>
2023-12-04 10:54:46 +0900

    어노테이션
    11

--- src/main/java/com/mkfactory/toothless/e/onlineCounsel/controller/OnlineCounselController.java
+++ src/main/java/com/mkfactory/toothless/e/onlineCounsel/controller/OnlineCounselController.java
@@ -1,5 +1,8 @@
 package com.mkfactory.toothless.e.onlinecounsel.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class OnlineCounselController {
 
 }
--- src/main/java/com/mkfactory/toothless/e/onlinecounsel/service/OnlineCounselService.java
+++ src/main/java/com/mkfactory/toothless/e/onlinecounsel/service/OnlineCounselService.java
@@ -1,5 +1,8 @@
 package com.mkfactory.toothless.e.onlinecounsel.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class OnlineCounselService {
 
 }
