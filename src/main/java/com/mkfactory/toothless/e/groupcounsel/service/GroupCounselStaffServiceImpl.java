c75869f5b42324c24b02beeaa03afe7c4c940bcb
Pyeonggeun <rjal3054@gmail.com>
2023-12-04 11:32:51 +0900

    집단상담 등록

--- src/main/java/com/mkfactory/toothless/e/groupcounsel/service/GroupCounselStaffServiceImpl.java
+++ src/main/java/com/mkfactory/toothless/e/groupcounsel/service/GroupCounselStaffServiceImpl.java
@@ -1,14 +1,20 @@
 package com.mkfactory.toothless.e.groupcounsel.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service("groupCounselStaffServiceImpl")
+import com.mkfactory.toothless.e.dto.GroupCounselDto;
+import com.mkfactory.toothless.e.groupcounsel.mapper.GroupCounselStaffSqlMapper;
+
+@Service
 public class GroupCounselStaffServiceImpl {
 	
+	@Autowired
+	GroupCounselStaffSqlMapper groupCounselStaffMapper;
 	
-	public void groupCounselRegister() {
-		
-		
+	
+	public void groupCounselRegister(GroupCounselDto groupCounselDto) {
+		groupCounselStaffMapper.insertGroupCounsel(groupCounselDto);
 		
 	}
 	
