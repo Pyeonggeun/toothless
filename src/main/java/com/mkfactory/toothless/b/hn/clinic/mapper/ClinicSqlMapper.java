28359ccf5ac238f4902394dc390288963cc8e37b
01-hn <145423323+01-hn@users.noreply.github.com>
2023-12-07 17:18:04 +0900

    studentboardLikeDto 생성, Clinic 코드작업중, 서버이상무

--- src/main/java/com/mkfactory/toothless/b/hn/clinic/mapper/ClinicSqlMapper.java
+++ src/main/java/com/mkfactory/toothless/b/hn/clinic/mapper/ClinicSqlMapper.java
@@ -1,5 +1,16 @@
 package com.mkfactory.toothless.b.hn.clinic.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.mkfactory.toothless.b.dto.ClinicPatientDto;
+
 public interface ClinicSqlMapper {
+	
+	public List<ClinicPatientDto> getClinicPatientInfoList(
+			@Param("pageNumber") int pageNumber, @Param("searchWord") String searchWord);
+	public String getClinicPatientClassifyByResidentId(String resident_id);
+	public int getClinicPatientTotalPageNumber(String searchWord);
 	
 }
--- src/main/java/com/mkfactory/toothless/b/hn/clinic/service/ClinicServiceImpl.java
+++ src/main/java/com/mkfactory/toothless/b/hn/clinic/service/ClinicServiceImpl.java
@@ -1,8 +1,14 @@
 package com.mkfactory.toothless.b.hn.clinic.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mkfactory.toothless.b.dto.ClinicPatientDto;
 import com.mkfactory.toothless.b.hn.clinic.mapper.ClinicSqlMapper;
 
 @Service
@@ -10,5 +16,33 @@ public class ClinicServiceImpl {
 	
 	@Autowired
 	private ClinicSqlMapper clinicSqlMapper;
+	
+	public List<Map<String, Object>> getClinicPatientInfoList(int pageNumber, String searchWord) {
+		
+		List<Map<String, Object>> list = new ArrayList<>();
+		
+		List<ClinicPatientDto> clinicPatientInfoList = clinicSqlMapper.getClinicPatientInfoList(pageNumber, searchWord);
+		
+		for(ClinicPatientDto clinicPatientDto : clinicPatientInfoList) {
+			Map<String, Object> map = new HashMap<>();
+			String classify = clinicSqlMapper.getClinicPatientClassifyByResidentId(clinicPatientDto.getResident_id());
+			
+			if(classify == null) {
+				classify = "외부인";
+			}
+			
+			map.put("clinicPatientInfo", clinicPatientDto);
+			map.put("classify", classify);
+			
+			list.add(map);
+		}
+		
+		return list;
+	}
+	
+	public int getClinicPatientTotalPageNumber(String searchWord) {
+		
+		return clinicSqlMapper.getClinicPatientTotalPageNumber(searchWord);
+	}
 
 }
