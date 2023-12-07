699d6627c2cadf7442fac36f1ac03dfd96b4aeaa
Hooyeoung <145423023+Hooyeoung@users.noreply.github.com>
2023-12-07 14:55:44 +0900

    가볍게 윤문 및 추가

---
+++ src/main/java/com/mkfactory/toothless/c/huyeoung/controller/HuyeoungProfessorController.java
@@ -0,0 +1,16 @@
+package com.mkfactory.toothless.c.huyeoung.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mkfactory.toothless.c.huyeoung.service.HuyeoungProfessorServiceImpl;
+
+@Controller
+@RequestMapping("/tl_c/huyeong/*")
+public class HuyeoungProfessorController {
+		
+	
+	
+
+}
