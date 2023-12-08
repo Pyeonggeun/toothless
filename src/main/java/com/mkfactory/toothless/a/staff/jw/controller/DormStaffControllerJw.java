c2389a75a72d3bf6a8b057b5f7903d4414f3144e
baehanmil <118594337+StrngJW@users.noreply.github.com>
2023-12-08 16:45:11 +0900

    임원 페이지 -(임시)완-

--- src/main/java/com/mkfactory/toothless/a/staff/jw/controller/DormStaffControllerJw.java
+++ src/main/java/com/mkfactory/toothless/a/staff/jw/controller/DormStaffControllerJw.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mkfactory.toothless.a.staff.jw.service.DormStaffServiceJw;
 
@@ -28,12 +29,6 @@ public class DormStaffControllerJw {
 		return "/tl_a/staff/jw_callAttendSituationPage";
 	}
 	
-	@RequestMapping("jw_pointCategoryPage")
-	public String jw_pointCategoryPage() {
-		
-		return "/tl_a/staff/jw_pointCategoryPage";
-	}
-	
 	@RequestMapping("jw_pointManagementPage")
 	public String jw_pointManagementPage() {
 		
@@ -47,25 +42,66 @@ public class DormStaffControllerJw {
 	}
 	
 	@RequestMapping("jw_executiveAssignmentPage")
-	public String jw_executiveAssignmentPage() {
+	public String jw_executiveAssignmentPage(Model model) {
+		
+		model.addAttribute("dormStudentListMap", dormStaffServiceJw.getAllDormStudentList());
 		
 		return "/tl_a/staff/jw_executiveAssignmentPage";
 	}
 	
+	@RequestMapping("jw_deleteExecutiveProcess")
+	public String jw_deleteExecutiveProcess(int dorm_student_pk) {
+		
+		dormStaffServiceJw.deleteExecutive(dorm_student_pk);
+		
+		return "redirect:/tl_a/staff/jw_executiveAssignmentPage";
+	}
+	
+	
+	@RequestMapping("jw_executiveAssignmentProcess")
+	public String jw_executiveAssignmentProcess(int dorm_student_pk) {
+		
+		dormStaffServiceJw.registerExecutive(dorm_student_pk);
+		
+		return "redirect:/tl_a/staff/jw_executiveAssignmentPage";
+	}
+	
 	@RequestMapping("jw_executiveRoomAssignmentPage")
-	public String jw_executiveRoomAssignmentPage() {
+	public String jw_executiveRoomAssignmentPage(Model model) {
+		
+		model.addAttribute("executiveListMap", dormStaffServiceJw.getAllExecutiveList());
 		
 		return "/tl_a/staff/jw_executiveRoomAssignmentPage";
 	}
 	
+	@RequestMapping("jw_executiveRoomassignmentProcess")
+	public String jw_executiveRoomassignmentProcess(@RequestParam("dorm_room_pk") int dorm_room_pk, @RequestParam("executive_pk") int executive_pk) {
+		
+		dormStaffServiceJw.assignmentExecutiveRoom(dorm_room_pk, executive_pk);
+		
+		return "redirect:/tl_a/staff/jw_executiveRoomAssignmentPage";
+	}
+	
+	@RequestMapping("jw_executiveRoomRemoveProcess")
+	public String jw_executiveRoomRemoveProcess(@RequestParam("executive_pk") int executive_pk, @RequestParam("dorm_room_pk") int dorm_room_pk) {
+		
+		dormStaffServiceJw.removeExecutivePerRoom(executive_pk, dorm_room_pk);
+		
+		return "redirect:/tl_a/staff/jw_executiveRoomAssignmentPage";
+	}
+	
 	@RequestMapping("jw_executiveAssignmentSituationPage")
-	public String jw_executiveAssignmentSituationPage() {
+	public String jw_executiveAssignmentSituationPage(Model model) {
+		
+		model.addAttribute("executiveManagemenListMap", dormStaffServiceJw.getAllExecutiveManagementList());
 		
 		return "/tl_a/staff/jw_executiveAssignmentSituationPage";
 	}
 	
 	@RequestMapping("jw_diaryManagementPage")
-	public String jw_diaryManagementPage() {
+	public String jw_diaryManagementPage(Model model) {
+		
+		model.addAttribute("getAllDiaryList", dormStaffServiceJw.getAllDiaryList());
 		
 		return "/tl_a/staff/jw_diaryManagementPage";
 	}
