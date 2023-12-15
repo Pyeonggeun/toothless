682d3aeaec5cd521a877f1c90af2f247f0162260
sunmilk75 <145519399+sunmilk75@users.noreply.github.com>
2023-12-16 03:19:22 +0900

    성수_Staff_commonsUI
    dqw

--- src/main/java/com/mkfactory/toothless/e/onlinecounsel/service/OnlineCounselService.java
+++ src/main/java/com/mkfactory/toothless/e/onlinecounsel/service/OnlineCounselService.java
@@ -12,6 +12,9 @@ import com.mkfactory.toothless.donot.touch.dto.ExternalInfoDto;
 import com.mkfactory.toothless.donot.touch.dto.StudentInfoDto;
 import com.mkfactory.toothless.e.dto.CounselorDto;
 import com.mkfactory.toothless.e.dto.CounselorTypeDto;
+import com.mkfactory.toothless.e.dto.FreeboardDto;
+import com.mkfactory.toothless.e.dto.GroupCounselDto;
+import com.mkfactory.toothless.e.dto.NoticeBoardDto;
 import com.mkfactory.toothless.e.dto.OnlineCounselBoardDto;
 import com.mkfactory.toothless.e.dto.OnlineCounselReplyDto;
 import com.mkfactory.toothless.e.dto.OnlineCounselSurveyDto;
@@ -175,33 +178,22 @@ public class OnlineCounselService {
 	
 	
 	
-//
-//	
-//
-//	public CounselorDto getCounselorInfo(int externalPk) {
-//		
-//		return onlineCounselSqlMapper.selectCounselorInfo(externalPk);
-//		
-//		
-//	}
-//	
-//	public List<CounselorTypeDto> getCounselorTypeInfo (int id){
-//		
-//		return onlineCounselSqlMapper.selectCounselorTypeInfo(id);
-//		
-//	}
-//	
-//	
-//	public List<OnlineCounselBoardDto> getCounselBoardInfo(int typeCategoryId){
-//		
-//		
-//		return onlineCounselSqlMapper.selectCounselBoardInfo(typeCategoryId);
-//	}
-//	
-//	public StudentInfoDto getStudentInfo(int studentPk){
-//		
-//		return onlineCounselSqlMapper.getStudentInfo(studentPk);
-//		
-//	}
 	
+	
+	
+	// 공용 UI 용 메소드
+	public List<NoticeBoardDto> getNoticeListSpecificNum(){
+		
+		return onlineCounselSqlMapper.selectNoticeListSpecificNum();
+	}
+	
+	public List<FreeboardDto> getFreeboardListSpecificNum(){
+		
+		return onlineCounselSqlMapper.selectFreeboardListSpecificNum();
+	}
+	
+	public List<GroupCounselDto> getGroupCounselListSpecificNum(){
+		
+		return onlineCounselSqlMapper.selectGroupCounselListSpecificNum();
+	}
 }
