9f73502fb9ea4f93e226f8c99d50387127634f98
okc06058 <okc06058@naver.com>
2023-12-20 09:51:26 +0900

    cccc
    ccc

--- src/main/java/com/mkfactory/toothless/c/seoyoung/controller/SeoyoungCompanyController.java
+++ src/main/java/com/mkfactory/toothless/c/seoyoung/controller/SeoyoungCompanyController.java
@@ -36,10 +36,10 @@ public class SeoyoungCompanyController {
 	
 		ExternalInfoDto sessionCompanyInfo = (ExternalInfoDto)session.getAttribute("sessionExternalInfo");
 		model.addAttribute("sessionCompanyInfo", sessionCompanyInfo);
-//		System.out.println("-------------------------------------------");
+		System.out.println("-------------------------------------------");
 		
-//		System.out.println(sessionCompanyInfo.getExternal_pk());
-//		System.out.println("-------------------------------------------");
+		System.out.println(sessionCompanyInfo.getExternal_pk());
+		System.out.println("-------------------------------------------");
 		List<Map<String, Object>> map = (List<Map<String, Object>>) seoyoungCompanyService.companyList(sessionCompanyInfo.getExternal_pk());
 		model.addAttribute("list", map);
 		
