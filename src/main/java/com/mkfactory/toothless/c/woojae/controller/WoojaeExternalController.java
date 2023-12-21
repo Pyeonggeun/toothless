a7d8fe2cf0b348b0f0510d2c5be19cc6940d6d19
kimuzz <134681164+kimuzz@users.noreply.github.com>
2023-12-21 14:06:26 +0900

    공지사항

--- src/main/java/com/mkfactory/toothless/c/woojae/controller/WoojaeExternalController.java
+++ src/main/java/com/mkfactory/toothless/c/woojae/controller/WoojaeExternalController.java
@@ -2,11 +2,17 @@ package com.mkfactory.toothless.c.woojae.controller;
 
 
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mkfactory.toothless.c.dto.AjdksCompanyInfoDto;
+import com.mkfactory.toothless.c.taeho.service.TaehoExternalServiceImpl;
 import com.mkfactory.toothless.c.woojae.service.WoojaeStaffServiceImpl;
+import com.mkfactory.toothless.donot.touch.dto.ExternalInfoDto;
 
 
 @Controller
@@ -15,17 +21,31 @@ public class WoojaeExternalController {
 	
 	@Autowired
 	private WoojaeStaffServiceImpl woojaeStaffService;
+	@Autowired
+	private TaehoExternalServiceImpl taehoExternalService;	
 
 	@RequestMapping("ajdksInternshipCourseListPage")
-	public String ajdksInternshipCourseListPage() {
-	
+	public String ajdksInternshipCourseListPage(HttpSession session, Model model) {
+		
+		ExternalInfoDto externalInfoDto = (ExternalInfoDto)session.getAttribute("sessionExternalInfo");
+		int externalPk = externalInfoDto.getExternal_pk();
+		AjdksCompanyInfoDto ajdksCompanyInfoDto = taehoExternalService.getSessionCompanyInfoByExternalPk(externalPk);
+		model.addAttribute("ajdksCompanyInfoDto", ajdksCompanyInfoDto);
+		
 		
 		return"tl_c/woojae/external/ajdksInternshipCourseListPage";
 	}
 	
 	
 	@RequestMapping("ajdksExternalNoticePage")
-	public String ajdksExternalNoticePage() {
+	public String ajdksExternalNoticePage(HttpSession session, Model model) {
+		
+		
+		ExternalInfoDto externalInfoDto = (ExternalInfoDto)session.getAttribute("sessionExternalInfo");
+		int externalPk = externalInfoDto.getExternal_pk();
+		AjdksCompanyInfoDto ajdksCompanyInfoDto = taehoExternalService.getSessionCompanyInfoByExternalPk(externalPk);
+		model.addAttribute("ajdksCompanyInfoDto", ajdksCompanyInfoDto);
+		
 		
 		return"tl_c/woojae/external/ajdksExternalNoticePage";
 	}
